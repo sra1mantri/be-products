282f88cfdc1aaf12fdb382a7e4b69411197704ad
mantrisravan <mantri.sravan@nagarro.com>
2025-12-15 16:27:01 +0300

    Moving the price values to the application.properties file.

--- src/main/java/com/assignment/products/discount/OrderPriceDiscountStrategy.java
+++ src/main/java/com/assignment/products/discount/OrderPriceDiscountStrategy.java
@@ -1,6 +1,7 @@
 package com.assignment.products.discount;
 
 import com.assignment.products.entity.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -8,10 +9,16 @@ import java.math.BigDecimal;
 @Component
 public class OrderPriceDiscountStrategy implements DiscountStrategy{
 
+    @Value("${spring.application.order.discount.minimum-price}")
+    String minimumOrderPriceForDiscount;
+
+    @Value("${spring.application.order.discount.order-amount}")
+    String orderPriceDiscount;
+
     @Override
     public BigDecimal calculateDiscount(User user, BigDecimal orderTotal) {
-        if (orderTotal.compareTo(new BigDecimal("500.00")) > 0) {
-            return new BigDecimal("0.05");
+        if (orderTotal.compareTo(new BigDecimal(minimumOrderPriceForDiscount)) > 0) {
+            return new BigDecimal(orderPriceDiscount);
         }
         return BigDecimal.ZERO;
     }
--- src/main/java/com/assignment/products/discount/PremiumUserDiscountStrategy.java
+++ src/main/java/com/assignment/products/discount/PremiumUserDiscountStrategy.java
@@ -2,6 +2,7 @@ package com.assignment.products.discount;
 
 import com.assignment.products.entity.User;
 import com.assignment.products.enums.Role;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -9,10 +10,13 @@ import java.math.BigDecimal;
 @Component
 public class PremiumUserDiscountStrategy implements DiscountStrategy{
 
+    @Value("${spring.application.order.discount.premium-user}")
+    String premiumUserDiscount;
+
     @Override
     public BigDecimal calculateDiscount(User user, BigDecimal orderTotal) {
         if (user.getRole() == Role.PREMIUM_USER) {
-            return new BigDecimal("0.10");
+            return new BigDecimal(premiumUserDiscount);
         }
         return BigDecimal.ZERO;
     }
